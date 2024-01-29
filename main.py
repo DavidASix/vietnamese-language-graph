0d02d6fc3b4b789e982eb929eaa5ea01526ee046
David Anderson <davidandersonsix@gmail.com>
2024-01-29 13:37:32 -0500

    Added kebab case column. This is done to give opportunity for comparison in future functions.

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ import pandas as pd
 import xml.etree.ElementTree as ET
 
 
+
+
+
+# This is the original method to create the markdown files
 def reverse_search_method(df):
     # Split the words into morphemes
-    for index, row in df.iterrows():
+    for i, row in df.iterrows():
         morphemes = row['word'].split()
-        filename = '-'.join(morphemes) + '.md'
+        filename = row['kebab'] + '.md'
         connections = []
         # Loop the reversed morphemes to find possible phrases, then check their validity
         morphemes.reverse()
-        for index in range(len(morphemes)):
-            phrase_container = morphemes[0:index+1]
+        for j in range(len(morphemes)):
+            phrase_container = morphemes[0:j+1]
             phrase_container.reverse()
             phrase_container = '-'.join(phrase_container)
             connections.append(phrase_container)
@@ -23,16 +27,16 @@ def reverse_search_method(df):
             file.write(f"# {row['word']}\n\n")
             file.write(f"## Definition:\n\n")
             file.write(f"{row['definition']}\n\n")
-            file.write(f"Subwords:\n")
+            file.write(f"## Subwords:\n")
             for connection in connections:
                 file.write(f"- [[{connection}]]\n")
-        print(f"{filename} created - {index}")
+        print(f"{filename} created - {i}")
     print("Done")
 
 def parse_dictionary():
     tree = ET.parse('./resources/Viet_Anh.xml') 
     dictionary = tree.getroot()
-    df = pd.DataFrame(columns=['word', 'definition'])
+    df = pd.DataFrame(columns=['word', 'kebab', 'definition'])
     # Only parsing first 100 while testing
     dictionary = dictionary[0:100]
     for record in dictionary:
@@ -40,15 +44,16 @@ def parse_dictionary():
         definition_elm = record.find('meaning')
         if word_elm.text is not None and definition_elm is not None:
             word = word_elm.text.strip()
+            kebab = word_elm.text.strip().split()
+            kebab = '-'.join(kebab)
             definition = definition_elm.text.strip()
-            df.loc[len(df.index)] = [word, definition]
+            df.loc[len(df.index)] = [word, kebab, definition]
     return df
 
 
 def main():
     # Parse the dictionary to create a dataframe
     df = parse_dictionary()
-    print(df.head())
     # Write data to markdown files
     reverse_search_method(df)
 
